a7ee86ceda76c66cbe7eafb8a5faf678997ec39c
vexe <vexebusiness6472814@gmail.com>
2022-07-15 23:27:30 +0545

    initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,24 @@
+zoxide init fish | source
+
+alias ls "exa"
+alias la "ls -a"
+alias ll "ls -l"
+alias lal "ls -la"
+alias cls "clear"
+alias rm "rm -i"
+alias cd "z"
+
+alias config="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_greeting
+    echo "well cum"
+end
+
+function mkcd
+    mkdir $argv[1]
+    cd $argv[1]
+end
