6041aead0b1d9fcb91db49efebb749fb2b2f8891
vexe <vexebusiness6472814@gmail.com>
2022-07-16 12:23:35 +0545

    [added] some functions for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,11 @@
+# env variables
+set -x EDITOR "kak"
+set -x PAGER "less"
+
+# zoxide initialization
 zoxide init fish | source
 
+# aliases
 alias ls "exa"
 alias la "ls -a"
 alias ll "ls -l"
@@ -8,8 +14,7 @@ alias tree "ls -T"
 alias cls "clear"
 alias rm "rm -i"
 alias cd "z"
-
-alias config="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
+alias dotfiles="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
@@ -19,6 +24,21 @@ function fish_greeting
     echo "well cum"
 end
 
+# functions
+# function to compile and run a c or c++ source file
+function crun
+    if string match -qr ".c\$" $argv[1]
+        set OUT $(echo $argv[1] | awk '{ print substr( $0, 1, length($0)-2 ) }')
+        gcc $argv[1] -o $OUT && printf "Compilation complete, running...\n\n" && ./$OUT && rm -f $OUT
+    else if string match -qr ".cpp\$" $argv[1]
+        set OUT $(echo $argv[1] | awk '{ print substr( $0, 1, length($0)-4 ) }')
+        g++ $argv[1] -o $OUT && printf "Compilation complete, running...\n\n" && ./$OUT && rm -f $OUT
+    else
+        echo "Please input a C or C++ source file!"
+    end
+end
+
+# function to make a directory and switch to it
 function mkcd
     mkdir $argv[1]
     cd $argv[1]
